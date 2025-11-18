a033250043219ae4bddd77ee3076c37e7e6f2e10
KavindaAGKR <kavindaaludeniya@gmail.com>
2025-11-18 22:42:33 +0530

    spring initialized

---
+++ src/test/java/com/example/Kafka_Based_Ordering_System_with_Avro_3814/KafkaBasedOrderingSystemWithAvro3814ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kafka_Based_Ordering_System_with_Avro_3814;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBasedOrderingSystemWithAvro3814ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
